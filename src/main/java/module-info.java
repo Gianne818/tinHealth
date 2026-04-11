59083b6453dceb9960cdd82cb45f185aa35df1ba
Gianne <cruzgian008@gmail.com>
2026-04-11 12:33:50 +0800

    added NotificationTab

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -21,4 +21,7 @@ module org.tin.oop2_capstone {
     opens org.tin.oop2_capstone.screens.settings to javafx.fxml;
     exports org.tin.oop2_capstone.screens.settings to javafx.graphics;
 
+    opens org.tin.oop2_capstone.screens.notification_tab to javafx.fxml;
+    exports org.tin.oop2_capstone.screens.notification_tab to javafx.graphics;
+
 }
---
+++ src/main/java/org/tin/oop2_capstone/screens/notification_tab/NotificationTabController.java
@@ -0,0 +1,4 @@
+package org.tin.oop2_capstone.screens.notification_tab;
+
+public class NotificationTabController {
+}
