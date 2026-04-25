e4bc7e428b3e4d20d5b6805cb918af9694d4d5a6
Gianne <cruzgian008@gmail.com>
2026-04-26 03:34:46 +0800

    added fonts, semi-finalized navbar styles

--- src/main/java/org/tin/oop2_capstone/LoginController.java
+++ src/main/java/org/tin/oop2_capstone/LoginController.java
@@ -6,9 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class LoginController {
     @FXML public Button buttonLogin;
