28a0fb23cdc27ba4866d1764b0cd835cabf293b7
Vince <silvavincemathew@gmail.com>
2026-04-26 17:41:01 +0800

    added minimal ui such as hover effect to navButtons, made activity-log-view as well as resolving activity-log-view file not found, added login designs

--- src/main/java/org/tin/oop2_capstone/screens/activity_log/ActivityLogController.java
+++ src/main/java/org/tin/oop2_capstone/screens/activity_log/ActivityLogController.java
@@ -1,4 +1,13 @@
 package org.tin.oop2_capstone.screens.activity_log;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
 public class ActivityLogController {
+    private Button buttonAddActivity;
+    private ScrollPane activityLogScrollPane;
+
+    public void onButtonAddActivityClicked(ActionEvent actionEvent) {
+    }
 }
