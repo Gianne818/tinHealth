4cf7af954f6dbf59801b3a0aab68fe75d5ece951
macooyb <macoybacalla9@gmail.com>
2026-04-26 17:35:24 +0800

    first commit for new branchh

--- src/main/java/org/tin/oop2_capstone/MainApplication.java
+++ src/main/java/org/tin/oop2_capstone/MainApplication.java
@@ -3,6 +3,7 @@ package org.tin.oop2_capstone;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -19,19 +20,36 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/tin/oop2_capstone/views/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/tin/oop2_capstone/views/login-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+//
+//        primaryStage.setMinHeight(400);
+//        primaryStage.setMinWidth(600);
+//
+//        String style = getClass().getResource("/org/tin/oop2_capstone/styles/application.css").toExternalForm();
+//        scene.getStylesheets().add(style);
+//        primaryStage.setResizable(false);
+//
+//        primaryStage.setTitle("Health Tracker");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
 
-        primaryStage.setMinHeight(400);
-        primaryStage.setMinWidth(600);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/tin/oop2_capstone/views/main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
         String style = getClass().getResource("/org/tin/oop2_capstone/styles/application.css").toExternalForm();
         scene.getStylesheets().add(style);
-        primaryStage.setResizable(false);
+
+
+        primaryStage.setResizable(true);
+
+        primaryStage.setMinWidth(1000);
+        primaryStage.setMinHeight(800);
 
         primaryStage.setTitle("Health Tracker");
         primaryStage.setScene(scene);
-        primaryStage.show();
 
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 }
