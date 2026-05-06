78d63793ff63ac9504df92497725a905eea093b6
Ycany Sanchez <ycanyashra.sanchez2006@gmail.com>
2026-05-06 16:09:31 +0800

    Created PermissionService and added functions to RetrieveData and UpdateData which will be used by PermissionService
    Functions of PermissionService
    Checks internet
    syncs pending food

--- src/main/java/org/tin/oop2_capstone/database/UpdateData.java
+++ src/main/java/org/tin/oop2_capstone/database/UpdateData.java
@@ -1,4 +1,68 @@
 package org.tin.oop2_capstone.database;
 
+import org.tin.oop2_capstone.model.entities.NutritionDetails;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UpdateData {
+
+
+    public static void updateFoodNutrition(String foodName, NutritionDetails nutrition) {
+        String query = """
+            UPDATE NutritionalDetails nd
+            JOIN Consumables c ON c.nutri_id = nd.nutri_id
+            SET nd.calories     = ?,
+                nd.protein      = ?,
+                nd.fats         = ?,
+                nd.carbs        = ?,
+                nd.cholesterol  = ?,
+                nd.sodium       = ?,
+                nd.sugar        = ?,
+                nd.fiber        = ?
+            WHERE c.name = ?
+            AND c.type   = 'food'
+        """;
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setDouble(1, nutrition.getCalories());
+            stmt.setDouble(2, nutrition.getProtein());
+            stmt.setDouble(3, nutrition.getFat());
+            stmt.setDouble(4, nutrition.getCarbs());
+            stmt.setDouble(5, nutrition.getCholesterol());
+            stmt.setDouble(6, nutrition.getSodium());
+            stmt.setDouble(7, nutrition.getSugar());
+            stmt.setDouble(8, nutrition.getFiber());
+            stmt.setString(9, foodName);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void markAsSynced(String foodName) {
+        String query = """
+            UPDATE Consumables
+            SET is_synced = TRUE
+            WHERE name = ?
+            AND type = 'food'
+        """;
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, foodName);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
