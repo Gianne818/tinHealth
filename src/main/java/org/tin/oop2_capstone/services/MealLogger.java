d344137aeaae57b1a5bee914dd3e20a828240014
djames_pi <jamesbenitez341@gmail.com>
2026-05-27 22:06:56 +0800

    fixed that MealLogger class. na sayop pag merge sa intellij.

--- src/main/java/org/tin/oop2_capstone/services/MealLogger.java
+++ src/main/java/org/tin/oop2_capstone/services/MealLogger.java
@@ -13,44 +13,6 @@ import java.util.List;
 public class MealLogger extends Logger<MealLogObserver> {
     private static MealLogger instance;
     private MealLog mealLog;
-    private final List<MealLogObserver> observers;
-    private MealRepository mealRepository = DependencyService.getMealRepository();
-    private int userID = SessionManager.getInstance().getCurrentUser().getUid();
-
-    private Meal mealToAdd;
-
-
-    private MealLogger() {
-        this.mealLog = new MealLog();
-        this.observers = new ArrayList<>();
-    }
-
-    public static synchronized MealLogger getInstance() {
-        if (instance == null) {
-            instance = new MealLogger();
-        }
-        return instance;
-    }
-
-    public boolean addMeal(Meal meal){
-        this.mealToAdd = meal;
-        return logData();
-    }
-
-
-import org.tin.oop2_capstone.database.repositories.MealRepository;
-import org.tin.oop2_capstone.model.entities.Activity;
-import org.tin.oop2_capstone.model.entities.Meal;
-import org.tin.oop2_capstone.model.entities.MealLog;
-import org.tin.oop2_capstone.model.observer.MealLogObserver;
-import org.tin.oop2_capstone.model.observer.MealLogObserver;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MealLogger extends Logger<MealLogObserver> {
-    private static MealLogger instance;
-    private MealLog mealLog;
     private MealRepository mealRepository = DependencyService.getMealRepository();
     private int userID = SessionManager.getInstance().getCurrentUser().getUid();
 
@@ -113,4 +75,3 @@ public class MealLogger extends Logger<MealLogObserver> {
     }
 
 }
-
